d69be516822a4edffbc886c86bc87f5d0b5994de
lizhao <lizhao@yazuo.com>
2018-10-01 10:48:09 +0800

    init

---
+++ src/main/java/hosolu/bakend/BakendApplication.java
@@ -0,0 +1,12 @@
+package hosolu.bakend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakendApplication.class, args);
+	}
+}
